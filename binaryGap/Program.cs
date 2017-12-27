ae83b821d40cd582c2f666e39ae27d1320c1da15
Emanuel Suriano <emanuel.suriano@gmail.com>
2017-12-27 00:32:27 -0300

    adding binaryGap program

---
+++ binaryGap/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace binaryGap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = solution(328);
+            Console.WriteLine(result);
+        }
+
+        static int solution(int N)
+        {
+            var binaryNumber = Convert.ToString(N, 2);
+            var result = 0;
+            var count = 0;
+            bool startCounting = false;
+            foreach (var bit in binaryNumber)
+            {
+                if (bit == '0' && startCounting)
+                {
+                    count++;
+                    continue;
+                }
+
+                if (bit == '1')
+                {
+                    startCounting = true;
+
+                    if (count > 0)
+                    {
+                        if (count > result) result = count;
+                        count = 0;
+                    }
+                }
+
+            }
+            return result;
+        }
+    }
+}
