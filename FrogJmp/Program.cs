4dae5511e6cdf8f5acb3baf5ea1530d39882024b
Emanuel Suriano <emanuel.suriano@gmail.com>
2017-12-28 23:19:32 -0300

    Adding FrogJmp

---
+++ FrogJmp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FrogJmp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = solution(10, 85, 30);
+
+            Console.WriteLine(result);
+        }
+
+        static public int solution(int X, int Y, int D) {
+            var diff = Y - X;
+            var divider = Decimal.Divide(diff, D);
+            return (int)Math.Ceiling(divider);
+        }
+    }
+}
