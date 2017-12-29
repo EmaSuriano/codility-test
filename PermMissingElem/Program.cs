cf6e2dd5368affe7a5d58a6d66d946bae82ab61e
Emanuel Suriano <emanuel.suriano@gmail.com>
2017-12-28 23:38:56 -0300

    Adding PermMissingElem

---
+++ PermMissingElem/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PermMissingElem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myEmptyArray = new int[] {2, 3 };
+            var result = solution(myEmptyArray);
+
+            Console.WriteLine(result);
+        }
+
+        static public int solution(int[] A) {
+            var matchesDictionary = new Dictionary<int, bool>();
+            foreach (var number in A)
+            {
+                matchesDictionary.Add(number, true);
+            }
+            
+            int result = -1;
+            for (int i = 1; i < A.Length + 2; i++)
+            {
+                if(!matchesDictionary.ContainsKey(i)) {
+                    result = i;
+                    break;
+                }
+            }
+            
+            return result;
+    }
+    }
+}
